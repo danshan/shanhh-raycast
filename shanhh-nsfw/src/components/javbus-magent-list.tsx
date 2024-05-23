798a597908402170083f4d223dd791318aee6287
Honghao SHAN 单弘昊 <honghao.shan@nio.com>
2024-05-23 13:18:55 +0800

    feature update tags for nsfw

--- shanhh-nsfw/src/components/javbus-magent-list.tsx
+++ shanhh-nsfw/src/components/javbus-magent-list.tsx
@@ -23,9 +23,7 @@ export function JavbusMagnetList(props: { detail?: JavbusDetailData }) {
           ) :
           (magnets || []).map((result: JavbusMagnet) => (
             <List.Item key={result.magnet} title={result.title} icon={{ source: Icon.Link, tintColor: Color.Blue }}
-                       accessories={[
-                         { text: result.size, icon: { source: Icon.Document } },
-                         { text: result.date, icon: { source: Icon.Calendar } }]}
+                       accessories={buildAccessories({ result })}
                        actions={
                          <ActionPanel>
                            <Action.CopyToClipboard title="Copy Magnet" content={result.magnet} />
@@ -35,4 +33,20 @@ export function JavbusMagnetList(props: { detail?: JavbusDetailData }) {
       }
     </List>
   );
+}
+
+function buildAccessories(props: { result: JavbusMagnet }) {
+  const accessories: List.Item.Accessory[] = [];
+
+  if (props.result.tags && props.result.tags.length > 0) {
+    props.result.tags.forEach((tag) => {
+      accessories.push({ text: tag, icon: { source: Icon.Tag, tintColor: tag === "字幕" ? Color.Red : Color.Orange } });
+    });
+  }
+
+  accessories.push({ text: props.result.size, icon: { source: Icon.Document } });
+  accessories.push({ text: props.result.date, icon: { source: Icon.Calendar } });
+
+
+  return accessories;
 }
